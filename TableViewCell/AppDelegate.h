18e8404f76d2749cd3f844163d73566b6b7fb938
iMac <imac@zhulianshengdeimac.local>
2016-11-29 12:57:43 +0800

    Initial Commit

---
+++ TableViewCell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewCell
+//
+//  Created by iMac on 16/11/29.
+//  Copyright © 2016年 golddatacommunications. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableViewCell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableViewCell
+//
+//  Created by iMac on 16/11/29.
+//  Copyright © 2016年 golddatacommunications. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
